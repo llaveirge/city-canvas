55bdc4b42cce4cdec4b55f4d548c3a2ee0dc0c4a
Lindsey Laveirge <llaveirge@gmail.com>
2022-02-22 13:42:35 -0700

    Add a button to center the map based on user location using geolocation. #2

--- client/components/new-pin-map.jsx
+++ client/components/new-pin-map.jsx
@@ -4,7 +4,6 @@ import {
   GoogleMap,
   useLoadScript,
   Marker
-  // InfoWindow
 } from '@react-google-maps/api';
 
 const mapContStyle = {
@@ -12,14 +11,49 @@ const mapContStyle = {
   height: '320px'
 };
 
-const center = { lat: 39.7392, lng: -104.9903 };
+const center = { lat: 39.744137, lng: -104.950050 };
 
 export default function NewPinMap() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
   });
 
+  // Set state:
   const [marker, setMarker] = React.useState({});
+
+  // Set a custom marker via click:
+  const onMapClick = React.useCallback(event => {
+    setMarker({
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng()
+    });
+  }, []);
+
+  // Prevent re-renders with useRef, specifically when placing markers;
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback(map => {
+    mapRef.current = map;
+  }, []);
+
+  // Pan to a location:
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(17);
+  }, []);
+
+  function GeoLocate({ panTo }) {
+    return (
+      <button onClick={() => {
+        navigator.geolocation.getCurrentPosition(position => {
+          panTo({
+            lat: position.coords.latitude, lng: position.coords.longitude
+          });
+        }, () => null);
+      }}>
+        <img className='target' src='/target-audience.png' alt="Target my location!"/>
+      </button>
+    );
+  }
 
   if (loadError) return 'Error loading map';
   if (!isLoaded) return 'Loading Map';
@@ -28,16 +62,15 @@ export default function NewPinMap() {
   <div>
     <GoogleMap
       mapContainerStyle={mapContStyle}
-      zoom={16}
+      zoom={10}
       center={ center }
-      onClick={event => {
-        setMarker({
-          lat: event.latLng.lat(),
-          lng: event.latLng.lng()
-        });
-      }}>
+      onClick={onMapClick}
+      onLoad={onMapLoad}
+      >
 
-      <Marker position={{ lat: marker.lat, lng: marker.lng }}
+      <GeoLocate panTo={panTo} />
+
+      <Marker position={{ lat: +marker.lat, lng: +marker.lng }}
       icon={{
         url: '/pt_pin_sm.png',
         scaledSize: new window.google.maps.Size(35, 35)
