57118d15eafd555761605a56fc60e0748db4e180
Lindsey Laveirge <llaveirge@gmail.com>
2022-02-23 17:34:31 -0700

    Update Home component to include a fetch GET request from '/api/home-feed' endpoint and store post data from 'posts' and user data from 'users' database tables in the state arrays and verify that 'Card' components can render post previews with user and pin data from state data. #5

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -6,42 +6,16 @@ export default class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pins: [{
-        artPhotoUrl: 'https://images.unsplash.com/photo-1624198376649-c121a452d157?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        artistName: 'Another Guy',
-        postId: 1,
-        reported: false,
-        title: 'Rad Painting',
-        userId: 1
-      },
-      {
-        artPhotoUrl: 'https://images.unsplash.com/photo-1628064009888-ef9e3d2d14c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
-        artistName: 'Different Person',
-        postId: 2,
-        reported: false,
-        title: 'Even More Rad Painting',
-        userId: 2
-      }],
-      users: [{
-        userId: 1, profileUrl: 'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-      },
-      {
-        userId: 2, profileUrl: 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2034&q=80'
-      }
-      ]
+      pins: []
     };
-
-    this.matchUser = this.matchUser.bind(this);
   }
 
-  matchUser(pin) {
-    const id = pin.userId;
-    const users = this.state.users;
-    for (const user of users) {
-      if (user.userId === id) {
-        return user.profileUrl;
-      }
-    }
+  componentDidMount() {
+    fetch('/api/home-feed')
+      .then(response => response.json())
+      .then(pins => {
+        this.setState({ pins });
+      });
   }
 
   render() {
@@ -49,15 +23,15 @@ export default class Home extends React.Component {
 
     return (
       <Container className='feed-cont'>
-        <Row className='pt-2'>
+        <Row className='pt-5'>
           <Col className='justify-content-center'>
             {pins.map(pin => (
               <PostCard
-              key={pin.postId}
-              title={pin.title}
-              artPhotoUrl={pin.artPhotoUrl}
-              profileUrl={ this.matchUser(pin) }
-              artistName={pin.artistName}
+              key={ pin.postId }
+              title={ pin.title }
+              artPhotoUrl={ pin.artPhotoUrl }
+              profileUrl={ pin.photoUrl }
+              artistName={ pin.artistName }
               button='View More'
               />
             ))}
--- database/data.sql
+++ database/data.sql
@@ -23,8 +23,8 @@ insert into "posts" (
 	"lng",
 	"userId"
 )
-values ('Cool Thing', 'That One Guy', 'https://images.unsplash.com/photo-1624198376649-c121a452d157?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80', 'Saw this and it was really cool.', 42.361145, -71.057083, 1 ),
-  ('Look Up', 'Some Person', 'https://images.unsplash.com/photo-1608317024553-9769b09329cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80', 'Interesesting work.', 42.3587999, -71.0707389, 1 ),
+values ('Are You Daft?', '@DifferentGuy', 'https://images.unsplash.com/photo-1628064009888-ef9e3d2d14c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80', 'The best mural.', 42.3587999, -71.0707389, 2 ),
+  ('Cool Thing', 'That One Guy', 'https://images.unsplash.com/photo-1624198376649-c121a452d157?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80', 'Saw this and it was really cool.', 42.361145, -71.057083, 1 ),
+  ('Look Up', 'Some Person', 'https://images.unsplash.com/photo-1608317024553-9769b09329cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80', 'Interesesting work.', 42.3587999, -71.0707389, 3 ),
   ('Makey-Makey', 'Unknown', 'https://images.unsplash.com/photo-1633185077583-45eac5a73c80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80', 'Interesesting sculpture.', 42.3587999, -71.0707389, 2 ),
-  ('Mural Awesome', 'Namey McNamerson', 'https://images.unsplash.com/photo-1561149872-9e581fb713d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1927&q=80', 'Big mural that also happens to be awesome.', 42.3448902, -71.0866892, 3 ),
-  ('Are You Daft?', '@DifferenGuy', 'https://images.unsplash.com/photo-1628064009888-ef9e3d2d14c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80', 'The best mural.', 42.3587999, -71.0707389, 2 );
+  ('Mural Awesome', 'Namey McNamerson', 'https://images.unsplash.com/photo-1561149872-9e581fb713d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1927&q=80', 'Big mural that also happens to be awesome.', 42.3448902, -71.0866892, 3 );
