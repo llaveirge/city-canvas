1f8ed7e40305cc794566300a4cc9aa3ab4b8ca26
Lindsey Laveirge <llaveirge@gmail.com>
2022-03-07 22:34:06 -0700

    Add a geolocation button to the ArtFinder component, and load map with location centered near user. #14

--- client/pages/art-finder.jsx
+++ client/pages/art-finder.jsx
@@ -3,10 +3,9 @@ import { Navbar } from 'react-bootstrap';
 import {
   GoogleMap,
   useLoadScript,
-  Marker
+  Marker,
+  InfoWindow
 } from '@react-google-maps/api';
-
-const center = { lat: 38.836419, lng: -104.8276377 };
 
 export default function ArtFinder(props) {
   const { isLoaded, loadError } = useLoadScript({
@@ -15,9 +14,10 @@ export default function ArtFinder(props) {
 
   // const [markers, setMarkers] = React.useState([]);
 
-  const markers = [{ postId: 3, artPhotoUrl: 'https://www.uncovercolorado.com/wp-content/uploads/2020/12/colorado-springs-murals-take-back-the-power.jpg', lat: 38.834158324677844, lng: -104.82302912287078 },
-    { postId: 7, artPhotoUrl: 'https://bloximages.newyork1.vip.townnews.com/gazette.com/content/tncms/assets/v3/editorial/1/11/111fbde2-a061-11ea-ad32-bfb117588960/5ecedbafd9e1f.image.jpg?crop=1637%2C1228%2C0%2C19&resize=1637%2C1228&order=crop%2Cresize', lat: 38.83387105305894, lng: -104.82266124016441 }];
-  // const [selected, setSelected] = React.useState(null);
+  const markers = [{ postId: 3, img: 'https://www.uncovercolorado.com/wp-content/uploads/2020/12/colorado-springs-murals-take-back-the-power.jpg', lat: 38.834158324677844, lng: -104.82302912287078 },
+    { postId: 7, img: 'https://bloximages.newyork1.vip.townnews.com/gazette.com/content/tncms/assets/v3/editorial/1/11/111fbde2-a061-11ea-ad32-bfb117588960/5ecedbafd9e1f.image.jpg?crop=1637%2C1228%2C0%2C19&resize=1637%2C1228&order=crop%2Cresize', lat: 38.83387105305894, lng: -104.82266124016441 }];
+
+  const [selected, setSelected] = React.useState(null);
 
   // Prevent re-renders with useRef, specifically when placing/accessing/modifying markers:
   const mapRef = React.useRef();
@@ -25,37 +25,93 @@ export default function ArtFinder(props) {
     mapRef.current = map;
   }, []);
 
+  // Pan to a location:
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(13);
+  }, []);
+
   // useEffect(() => {
   //   fetch('/api/art-finder')
   //   .then()
   // })
 
+  // Use Geolocation to Locate the user for targeting via a button:
+  function GeoLocate({ panTo }) {
+    return (
+      <button type='button' onClick={() => {
+        navigator.geolocation.getCurrentPosition(position => {
+          panTo({
+            lat: position.coords.latitude, lng: position.coords.longitude
+          });
+        }, () => null);
+      }}>
+        <img className='target pin-pg'
+          src='/target-audience.png'
+          alt='Target my location!'
+          />
+      </button>
+    );
+  }
+
+  if (loadError) return 'Error loading map';
+  if (!isLoaded) return 'Loading map, one moment...';
+
+  const nearMe = navigator.geolocation.getCurrentPosition(position => {
+    panTo({
+      lat: position.coords.latitude, lng: position.coords.longitude
+    });
+  }, () => null);
+
   if (loadError) return 'Error loading map';
   if (!isLoaded) return 'Loading map, one moment...';
 
   return (
+
     <>
       <div>
         <GoogleMap
           mapContainerClassName='pin-map'
-          zoom = { 5 }
-          center={ center }
+          zoom = { 13 }
+          center={ nearMe}
           onLoad= {onMapLoad}
           >
-            {markers.map(marker => (
-              <Marker
-                key={marker.postId}
-                position={{ lat: marker.lat, lng: marker.lng }}
-                icon={{
-                  url: '/pt_pin_sm.png',
-                  scaledSize: new window.google.maps.Size(40, 40),
-                  anchor: new window.google.maps.Point(25, 40)
-                }}
-                // onClick={() => {
-                //   setSelected(marker);
-                // }}
-                />
-            ))}
+
+          <GeoLocate panTo={ panTo } />
+
+          {markers.map(marker => (
+            <Marker
+              key={marker.postId}
+              position={{ lat: marker.lat, lng: marker.lng }}
+              icon={{
+                url: '/pt_pin_sm.png',
+                scaledSize: new window.google.maps.Size(45, 45),
+                anchor: new window.google.maps.Point(22, 30)
+              }}
+              onClick={() => {
+                setSelected(marker);
+              }}
+              />
+          ))}
+
+            {selected
+              ? (<InfoWindow
+              position={{ lat: selected.lat, lng: selected.lng }}
+              onCloseClick={() => { setSelected(null); }}>
+                <div>
+                  <div className='info-img-cont'>
+                    <a href={`#pins?postId=${selected.postId}`}>
+                      <img className='info-img' src={selected.img}></img>
+                    </a>
+                  </div>
+                  <p className='text-center dir-link pt-1'>
+                    <a href={`https://www.google.com/maps/search/?api=1&query=${selected.lat}%2C${selected.lng}`}>
+                      Get Directions
+                    </a>
+                  </p>
+              </div>
+            </InfoWindow>)
+              : null}
 
         </GoogleMap>
       </div>
