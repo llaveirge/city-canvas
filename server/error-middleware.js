0fe32bd6917c66f22cb77afda575244553066f6c
Lindsey Laveirge <llaveirge@gmail.com>
2022-03-08 19:24:45 -0700

    Handled 23503 error in middleware. #11

--- server/error-middleware.js
+++ server/error-middleware.js
@@ -5,6 +5,11 @@ function errorMiddleware(err, req, res, next) {
     res.status(err.status).json({
       error: err.message
     });
+  } else if (err.code === '23503') {
+    console.error(err);
+    res.status(404).json({
+      error: "This isn't the pin you're looking for..."
+    });
   } else {
     console.error(err);
     res.status(500).json({
