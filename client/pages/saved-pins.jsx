885eacac8f51be3019ddbb8963eeea3d44f86dae
Lindsey Laveirge <llaveirge@gmail.com>
2022-03-11 19:43:29 -0700

    Add state to the 'SavedPins' page component and add dummy data and update render method to render 'PostCard' components from state data. #12

--- client/pages/saved-pins.jsx
+++ client/pages/saved-pins.jsx
@@ -3,33 +3,48 @@ import { Container, Col, Row } from 'react-bootstrap';
 import PostCard from '../components/card';
 
 export default class SavedPins extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pins: [
+        {
+          artPhotoUrl: 'https://bloximages.newyork1.vip.townnews.com/gazette.com/content/tncms/assets/v3/editorial/1/11/111fbde2-a061-11ea-ad32-bfb117588960/5ecedbafd9e1f.image.jpg?crop=1637%2C1228%2C0%2C19&resize=1637%2C1228&order=crop%2Cresize',
+          artistName: 'Matthew Carlson',
+          lat: 38.83387105305894,
+          lng: -104.82266124016441,
+          photoUrl: 'https://i.redd.it/gikb0vjg40651.jpg',
+          postId: 7,
+          reported: false,
+          saved: '2022-03-11T19:24:49.044Z',
+          saver: 1,
+          title: 'Guardian',
+          userId: 1,
+          UserName: 'TheRealSlimBaggins'
+        }
+      ]
+    };
+  }
 
   render() {
+    const { pins } = this.state;
+
     return (
       <Container className='feed-cont'>
           <h3 className='head-text pri-color mt-3 py-3'>My Saved City Canvas</h3>
         <Row className='pt-2'>
           <Col className='justify-content-center'>
-            <PostCard
-              key={ 3 }
-              title={ 'Look Up' }
-              artPhotoUrl={ 'https://images.unsplash.com/photo-1608317024553-9769b09329cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80' }
-              profileUrl={ 'https://qph.fs.quoracdn.net/main-qimg-7cfed85bb8a41d239fae5d82fece79b1' }
-              artistName={ 'Unknown' }
-              button='View More'
-              href={'#pins?postId=3'}
-              saved={ '2022-03-11T19:06:07.326574Z' }
-              />
+            {pins.map(pin => (
               <PostCard
-              key={ 5 }
-              title={ 'Mother' }
-              artPhotoUrl={ 'https://images.unsplash.com/photo-1608317024553-9769b09329cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80' }
-              profileUrl={ 'https://i.pinimg.com/236x/0c/3f/8f/0c3f8f47f143a37f8e53fa4bae4786f8--lord-rings-lord-of-the-rings.jpg' }
-              artistName={ 'L.N. Naga' }
+              key={ pin.postId }
+              title={ pin.title }
+              artPhotoUrl={ pin.artPhotoUrl }
+              profileUrl={ pin.photoUrl }
+              artistName={ pin.artistName }
               button='View More'
-              href={'#pins?postId=5'}
-              saved={ '2022-03-11T19:29:09.201163Z' }
+              href={`#pins?postId=${pin.postId}`}
+              saved={ pin.saved }
               />
+            ))}
           </Col>
         </Row>
       </Container>
