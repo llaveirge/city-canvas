431062c31616e505702599586e8c3b44f4ae7587
Lindsey Laveirge <llaveirge@gmail.com>
2022-03-12 22:58:18 -0700

    Display a message if there are no saved pins. #12

--- client/pages/saved-pins.jsx
+++ client/pages/saved-pins.jsx
@@ -26,18 +26,24 @@ export default class SavedPins extends React.Component {
           <h3 className='head-text pri-color mt-3 py-3'>My Saved City Canvas</h3>
         <Row className='pt-2'>
           <Col className='justify-content-center'>
-            {pins.map(pin => (
-              <PostCard
-              key={ pin.postId }
-              title={ pin.title }
-              artPhotoUrl={ pin.artPhotoUrl }
-              profileUrl={ pin.photoUrl }
-              artistName={ pin.artistName }
-              button='View More'
-              href={`#pins?postId=${pin.postId}`}
-              saved={ pin.saved }
-              />
-            ))}
+            {pins.length
+              ? pins.map(pin => (
+                <PostCard
+                  key={ pin.postId }
+                  title={ pin.title }
+                  artPhotoUrl={ pin.artPhotoUrl }
+                  profileUrl={ pin.photoUrl }
+                  artistName={ pin.artistName }
+                  button='View More'
+                  href={`#pins?postId=${pin.postId}`}
+                  saved={ pin.saved }
+                />
+              ))
+              : <h5 className='pri-color text-center font-weight-bold'>
+                  Nothing to see here...
+                  <br/>Browse the <a className='sec-color no-decoration' href='#'>City Canvas Home feed</a> and save your favorite pins!
+                </h5>
+              }
           </Col>
         </Row>
       </Container>
