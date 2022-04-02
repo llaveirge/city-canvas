904fc9727d79073dc576532e6e592da5c59ac442
Lindsey Laveirge <llaveirge@gmail.com>
2022-04-01 19:30:08 -0600

    Style 'Registration' page component according to wireframe. #15

--- client/pages/registration.jsx
+++ client/pages/registration.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-// import { Container } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 export default class Registration extends React.Component {
   render() {
     return (
       <>
-      <div className='registration-cont'>
-        <h1> Registration Page </h1>
+      <div className='pg-registration'>
+        <Container className='cont-registration py-5 py-sm-0'>
+          <Row className='bg-white'>
+              <h1 className='head-text pri-color py-5 text-center'>
+                Create an Account
+              </h1>
+          </Row>
+        </Container>
       </div>
-
       </>
     );
   }
