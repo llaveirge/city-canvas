5a57444cec4cff0c41e3a344a2b74ea33dfafcaf
Lindsey Laveirge <llaveirge@gmail.com>
2022-04-25 23:46:52 -0600

    Update 'AppNav' component so that 'Nav' options collapse when app is being used on small screens utilizing 'Navbar.Toggle' and 'Navbar.Collapse' React Boostrap elements. #15

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -9,23 +9,26 @@ export default class AppNav extends React.Component {
     const { user, handleSignOut } = this.context;
 
     return (
-    <Navbar variant='dark' className='pri-bk-color'>
+    <Navbar variant='dark' className='pri-bk-color' expand='sm'>
 
       <Container>
         <Navbar.Brand className='head-text menu' href='#'>Home</Navbar.Brand>
-          <Nav className='me-auto'>
-            <NavDropdown title='My Canvas'>
-              <NavDropdown.Item className='nav-drop' href='#my-canvas'>
-                My Canvas Pins
-              </NavDropdown.Item>
-              <NavDropdown.Item className='nav-drop' href='#my-saved-pins'>
-                My Saved Pins
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href='#art-finder'>Art Finder</Nav.Link>
-            { user ? <Nav.Link href='' onClick={handleSignOut}>Sign Out</Nav.Link> : null
-              }
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className='me-auto'>
+              <NavDropdown title='My Canvas'>
+                <NavDropdown.Item className='nav-drop' href='#my-canvas'>
+                  My Canvas Pins
+                </NavDropdown.Item>
+                <NavDropdown.Item className='nav-drop' href='#my-saved-pins'>
+                  My Saved Pins
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href='#art-finder'>Art Finder</Nav.Link>
+                { user ? <Nav.Link href='' onClick={handleSignOut}>Sign Out</Nav.Link> : null
+                  }
+            </Nav>
+          </Navbar.Collapse>
       </Container>
 
     </Navbar>
