89ff52d12b864fd99062acf447726a1640b43fee
Lindsey Laveirge <llaveirge@gmail.com>
2022-07-14 20:33:04 -0600

    Created 'loading-spinner' component to display to users that API fetch calls are loading and added the spinner to 'My Saved Pins' page. #33

---
+++ client/components/loading-spinner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+// import '../../server/public/spinner.css';
+
+export default function LoadingSpinner(props) {
+  return (
+    <Container>
+      <Row className='justify-content-center'>
+    <div className='lds-ellipsis'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    </Row>
+    </Container>
+  );
+}
--- client/pages/saved-pins.jsx
+++ client/pages/saved-pins.jsx
@@ -3,28 +3,31 @@ import { Container, Col, Row } from 'react-bootstrap';
 import PostCard from '../components/card';
 import AppContext from '../lib/app-context';
 import Redirect from '../components/redirect';
+import LoadingSpinner from '../components/loading-spinner';
 
 export default class SavedPins extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pins: []
+      pins: [],
+      isLoading: false
     };
   }
 
   componentDidMount() {
     const { user } = this.context;
     if (user) {
+      this.setState({ isLoading: true });
       fetch(`/api/saved-pins/${user.userId}`)
         .then(response => response.json())
         .then(pins => {
-          this.setState({ pins });
+          this.setState({ pins: pins, isLoading: false });
         });
     }
   }
 
   render() {
-    const { pins } = this.state;
+    const { pins, isLoading } = this.state;
     const { user } = this.context;
 
     if (!user) return <Redirect to='registration' />;
@@ -34,26 +37,31 @@ export default class SavedPins extends React.Component {
           <h3 className='head-text pri-color mt-3 py-3'>My Saved City Canvas</h3>
         <Row className='pt-2'>
           <Col>
-            { pins.length
-              ? pins.map(pin => (
-                <PostCard
-                  key={ pin.postId }
-                  title={ pin.title }
-                  artPhotoUrl={ pin.artPhotoUrl }
-                  profileUrl={ pin.photoUrl }
-                  artistName={ pin.artistName }
-                  button='View More'
-                  href={ `#pins?postId=${pin.postId}` }
-                  reported={ pin.reported }
-                  saver={ pin.saver }
-                  userId={ user.userId }
-                />
-              ))
-              : <h6 className='saved-canvas-empty-heading pri-color text-center font-weight-bold'>
-                  Nothing to see here...
-                  <br/>Browse the <a className='sec-color no-decoration' href='#'>
-                    City Canvas Home feed</a> and save your favorite pins!
-                </h6>
+            { isLoading
+              // ? <Container >
+              ? <LoadingSpinner />
+              // </Container>
+              : pins.length
+                ? pins.map(pin => (
+                  <PostCard
+                    key={ pin.postId }
+                    title={ pin.title }
+                    artPhotoUrl={ pin.artPhotoUrl }
+                    profileUrl={ pin.photoUrl }
+                    artistName={ pin.artistName }
+                    button='View More'
+                    href={ `#pins?postId=${pin.postId}` }
+                    reported={ pin.reported }
+                    saver={ pin.saver }
+                    userId={ user.userId }
+                  />
+                ))
+                : <h6 className='saved-canvas-empty-heading pri-color text-center fw-bold'>
+                    Nothing to see here...
+                    <br/>Browse the <a className='sec-color no-decoration' href='#'>
+                      City Canvas Home feed</a> and save your favorite pins!
+                  </h6>
+
             }
           </Col>
         </Row>
