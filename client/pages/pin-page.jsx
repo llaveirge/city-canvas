dba17b463e7fdea0c627a676300bdb2ab72f1dd3
Lindsey Laveirge <llaveirge@gmail.com>
2022-08-03 19:29:18 -0600

    Refactored render method of 'pin-page' component to remove unnecessary conditional ternary controlling the loading spinner display in order to reduce potential bugs on the render method's return statement. #33

--- client/pages/pin-page.jsx
+++ client/pages/pin-page.jsx
@@ -233,86 +233,88 @@ export default class PinPage extends React.Component {
       );
     }
 
+    if (isLoading && !show) {
+      return (
+      <div className='pt-5'><LoadingSpinner /></div>
+      );
+    }
+
     return (
       <>
-        { isLoading && !show
-          ? <div className='pt-5'><LoadingSpinner /></div>
-          : <>
-              <Container className='d-flex pt-sm-5 pt-3 align-items-center pin-cont'>
-                <Image
-                  className='profile-pic sec-bk-color'
-                  src={ pin.photoUrl }
-                ></Image>
-                <p className='feature-font-sm mb-0 ms-3'>{ pin.userName }</p>
-              </Container>
+        <Container className='d-flex pt-sm-5 pt-3 align-items-center pin-cont'>
+          <Image
+            className='profile-pic sec-bk-color'
+            src={ pin.photoUrl }
+          ></Image>
+          <p className='feature-font-sm mb-0 ms-3'>{ pin.userName }</p>
+        </Container>
 
-              <Container className='mt-4 pin-cont'>
-                <Card className='flex-sm-row'>
-                  <Col>
-                    <Card.Img className='full-pin-img' src={ pin.artPhotoUrl } />
-                  </Col>
-                  <Col className='custom-basis'>
-                    <Card.Body>
-                      <Card.Title
-                        as='h4'
-                        className={
-                          `head-text pri-color py-2 ${pin.reported === true
-                            ? 'me-5 pe-1'
-                            : ''}`
-                        }
-                      >
-                        { pin.reported === true
-                          ? <span className='align-top warning absolute-right'>
-                              <i className='fas fa-exclamation fa-sm'></i>
-                              <i className='ms-1 fas fa-eye-slash fa-sm'></i>
-                            </span>
-                          : null }
-                        { pin.title }
-                      </Card.Title>
-                        <Card.Text className='fw-bold pri-color pb-sm-1'>
-                          Artist: { pin.artistName }
-                        </Card.Text>
-                        <Card.Link
-                          href={
-                            `#pin-map?pinId=${pin.postId}&lat=${pin.lat}&lng=${pin.lng}&img=${encodeURIComponent(pin.artPhotoUrl)}`
-                          }
-                          className='fw-bold sec-color feature-font no-decoration'
-                        >
-                          <i className='me-2 fas fa-map-marker-alt fa-lg'></i>
-                          On The Map
-                        </Card.Link>
-                        <Card.Text className='pt-4 pb-5'>
-                          { pin.comment }
-                        </Card.Text>
-                        { pin.reported === false
-                          ? <Card.Link
-                              role='button'
-                              className='ab-bottom grey report me-5'
-                              onClick={ this.handleShow }>
-                                Report as removed from view
-                            </Card.Link>
-                          : <Card.Text className='ab-bottom warning report mb-0 me-5 pe-1'>
-                              Reported as removed from view
-                            </Card.Text>
-                        }
-                        <Card.Link className="bg-white ab-bottom-right">
-                          <i className={ pin.saved === null
-                            ? 'grey not-saved fas fa-heart fa-lg'
-                            : 'sec-color saved fas fa-heart fa-lg' }
-                            onClick={ this.toggleSaved }></i>
-                        </Card.Link>
-                    </Card.Body>
-                  </Col>
-                </Card>
-              </Container>
+        <Container className='mt-4 pin-cont'>
+          <Card className='flex-sm-row'>
+            <Col>
+              <Card.Img className='full-pin-img' src={ pin.artPhotoUrl } />
+            </Col>
+            <Col className='custom-basis'>
+              <Card.Body>
+                <Card.Title
+                  as='h4'
+                  className={
+                    `head-text pri-color py-2 ${pin.reported === true
+                      ? 'me-5 pe-1'
+                      : ''}`
+                  }
+                >
+                  { pin.reported === true
+                    ? <span className='align-top warning absolute-right'>
+                        <i className='fas fa-exclamation fa-sm'></i>
+                        <i className='ms-1 fas fa-eye-slash fa-sm'></i>
+                      </span>
+                    : null }
+                  { pin.title }
+                </Card.Title>
+                  <Card.Text className='fw-bold pri-color pb-sm-1'>
+                    Artist: { pin.artistName }
+                  </Card.Text>
+                  <Card.Link
+                    href={
+                      `#pin-map?pinId=${pin.postId}&lat=${pin.lat}&lng=${pin.lng}&img=${encodeURIComponent(pin.artPhotoUrl)}`
+                    }
+                    className='fw-bold sec-color feature-font no-decoration'
+                  >
+                    <i className='me-2 fas fa-map-marker-alt fa-lg'></i>
+                    On The Map
+                  </Card.Link>
+                  <Card.Text className='pt-4 pb-5'>
+                    { pin.comment }
+                  </Card.Text>
+                  { pin.reported === false
+                    ? <Card.Link
+                        role='button'
+                        className='ab-bottom grey report me-5'
+                        onClick={ this.handleShow }>
+                          Report as removed from view
+                      </Card.Link>
+                    : <Card.Text className='ab-bottom warning report mb-0 me-5 pe-1'>
+                        Reported as removed from view
+                      </Card.Text>
+                  }
+                  <Card.Link className="bg-white ab-bottom-right">
+                    <i className={ pin.saved === null
+                      ? 'grey not-saved fas fa-heart fa-lg'
+                      : 'sec-color saved fas fa-heart fa-lg' }
+                      onClick={ this.toggleSaved }></i>
+                  </Card.Link>
+              </Card.Body>
+            </Col>
+          </Card>
+        </Container>
 
-              <ModalReport
-                show={ show }
-                onHide={ this.handleClose }
-                report={ this.reportPin }
-                isLoading={ isLoading }
-              />
-            </> }
+        <ModalReport
+          show={ show }
+          onHide={ this.handleClose }
+          report={ this.reportPin }
+          isLoading={ isLoading }
+        />
       </>
     );
   }
