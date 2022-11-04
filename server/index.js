6803310b6f7286a27978ead7bd2941b53b02fd4b
Lindsey Laveirge <llaveirge@gmail.com>
2022-11-03 19:13:48 -0600

    Installed dependencies to facilitate AWS S3 bucket storage for file uploads, including 'multer-s3' and 'aws-sdk/client-s3'. Updated 'uploadws-middleware.js' to implement 'multer-s3' and 'aws-sdk/client-s3', and tested middleware. Updated 'index.js' to implement new middleware without the use of 'sharp' to ensure images successfully uploaded to the bucket. Working on debugging 'file.location' inaccuracies, and how to implement 'sharp'. #35

--- server/index.js
+++ server/index.js
@@ -303,21 +303,41 @@ app.post('/api/post-pin', uploadsMiddleware, (req, res, next) => {
   }
 
   // Resize and compress image uploads using sharp:
-  const { filename: image } = req.file;
+  // console.log('req.file: ', req.file);
+  // // const { filename: image } = req.file;
+  // // console.log('image: ', image);
+  // console.log('filename: ', req.file.filename);
+  // console.log('destination: ', req.file.destination);
+  // console.log('location: ', req.file.location);
 
-  sharp(req.file.path)
-    .resize({ width: 1000, withoutEnlargement: true })
-    .rotate()
-    .jpeg({ force: false, mozjpeg: true })
-    .png({ force: false, quality: 70 })
-    .webp({ force: false, quality: 70 })
-    .toFile(
-      path.resolve(req.file.destination, 'resized', image)
-    )
-    .then(data => {
-      const url = `/images/resized/${req.file.filename}`;
+  // sharp(req.file.path)
+  //   .resize({ width: 1000, withoutEnlargement: true })
+  //   .rotate()
+  //   .jpeg({ force: false, mozjpeg: true })
+  //   .png({ force: false, quality: 70 })
+  //   .webp({ force: false, quality: 70 })
+  //   .toFile(
+  //     path.resolve(req.file.destination, 'resized', image)
+  //   )
+  //   .then(data => {
+  // const url = `/images/resized/${req.file.filename}`;
 
-      const sql = `
+  // sharp(req.file.location)
+  //   .resize({ width: 1000, withoutEnlargement: true })
+  //   .rotate()
+  //   .jpeg({ force: false, mozjpeg: true })
+  //   .png({ force: false, quality: 70 })
+  //   .webp({ force: false, quality: 70 })
+  //   .toFile(
+  //     path.resolve(req.file.location, 'resized', image)
+  //   )
+  //   .then(data => {
+
+  const imageUrlLong = req.file.location;
+  const imageUrl = imageUrlLong.replace(`${process.env.AWS_S3_BUCKET}.`, '');
+  // console.log('Updated location string: ', imageUrl);
+
+  const sql = `
       INSERT INTO "posts"
         (
           "title",
@@ -331,10 +351,11 @@ app.post('/api/post-pin', uploadsMiddleware, (req, res, next) => {
       VALUES ($1, $2, $3, $4, $5, $6, $7)
       RETURNING *;
     `;
-      const params = [title, artist, url, info, lat, lng, userId];
+  const params = [title, artist, imageUrl, info, lat, lng, userId];
 
-      return db.query(sql, params);
-    })
+  // return db.query(sql, params);
+  db.query(sql, params)
+    // })
     .then(response => {
       const [pin] = response.rows;
       res.status(201).json(pin);
